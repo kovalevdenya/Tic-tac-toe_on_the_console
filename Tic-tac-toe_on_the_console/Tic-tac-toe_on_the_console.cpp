436b15f811c49734d75646cb97c842c0f3f229f3
kovalevdenya <kovalevdenis01@mail.ru>
2023-09-04 15:58:34 +0700

    Добавить меню игры

--- Tic-tac-toe_on_the_console/Tic-tac-toe_on_the_console.cpp
+++ Tic-tac-toe_on_the_console/Tic-tac-toe_on_the_console.cpp
@@ -1,1 +1,36 @@
-﻿
+﻿#include<iostream>
+#include<string>
+
+void Menu() {
+	std::cout << "\t\t\t\t\t\t" << "Tic-Tac" << std::endl;
+	std::cout << "\tМеню:" << std::endl;
+	std::cout << "1. Начать играть" << std::endl;
+	std::cout << "2. Выход" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Введите номер и нажмите Enter..." << std::endl;
+}
+
+
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	while (true) {
+		Menu();
+		std::string numMenu;
+		std::cin >> numMenu;
+		while (numMenu != "1" && numMenu != "2") {
+			std::cout << "Ошибка: выбранного номера меню не существует" << std::endl << "Введите номер еще раз и нажмите Enter..." << std::endl;
+			std::cin >> numMenu;
+		}
+		if (numMenu == "1") {
+			std::string Player1, Player2;
+			std::cout << "Введите имя Игрока №1" << std::endl;
+			std::getline(std::cin, Player1);
+			std::cout << "Введите имя Игрока №2" << std::endl;
+			std::getline(std::cin, Player2);
+		}
+		else if (numMenu == "2") {
+			break;
+		}
+	}
+}
